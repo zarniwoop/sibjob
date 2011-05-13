75cb4ff33fa92911b8bc1721d3163088f212e7ac
Nate McKie <nate.mckie@asolutions.com>
2011-05-13 16:37:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sibjob::Application.config.secret_token = '387e69e1eeba8e1e6190d834a76d17f24b0f4b04f1619c8ef3f80d38156275aa2257a12657dbad81ad48ef66f2716d9dfa184207022810441308f20ec6f88ebb'
