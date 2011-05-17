0ed1533a3f770dd1417e364e5bcb76de71e33322
Nate McKie <nate.mckie@asolutions.com>
2011-05-16 20:26:47 -0500

    Attempting to fix rack version error on Dreamhost.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
-gem 'rack', '1.2.1'
+gem 'rack', '1.2.2'
 
 gem 'mysql2', '0.2.7'
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
+  ENV['GEM_PATH'] = '/home/fapupon/.gems' + ':/usr/lib/ruby/gems/1.8'
+end
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
