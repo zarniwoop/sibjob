a89cda43a4590a8aaf074655663612be3af27ec8
Nate McKie <nate.mckie@asolutions.com>
2011-06-08 17:08:12 -0500

    Moved logic for post-login redirect to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource_or_scope)
+    # looks like resource_or_scope might be the user object, could
+    # check type to determine where to send next
+    jobs_sibling_path(current_sibling)
+  end
+
   def build_date_from_params(field_name, params)
     Date.new(params["#{field_name.to_s}(1i)"].to_i,
          params["#{field_name.to_s}(2i)"].to_i,
