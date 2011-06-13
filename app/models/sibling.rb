5d643397b4254c835b6682121e353368f97a5264
Nate McKie <nate.mckie@asolutions.com>
2011-06-12 21:36:04 -0500

    Removed ability to sign up for alpha period

--- app/models/sibling.rb
+++ app/models/sibling.rb
@@ -20,8 +20,8 @@
 
 class Sibling < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :registerable, :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
